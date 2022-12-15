1a0fa7cdc3305248b8710c240389db8119456610
luis <6010ldaniel@gmail.com>
2022-12-14 22:02:24 -0300

    App.js

--- present/src/App.js
+++ present/src/App.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+
 import './home.css';
 
+function App() {
+  return (
+    <body>
 
-ReactDOM.render(
-  <BrowserRouter>
-      <Login />
-      <Catalogo />
-  </ BrowserRouter>
-  , document.getElementById('root'));
-registerServiceWorker();
+        <header>
+        <div id="root" className="content">
+       
+            <a href="home.html"><button class="boton"><span>Home</span></button></a>
 
+            <a href="./components/catalogo/index.js"><button class="boton">
+            <span>Catalogo</span></button></a>
+
+            <a href="login.html"><button class="boton"><span>Entrar</span></button></a>
+
+            <a href="cadastro.html"><button class="boton"><span>Cadastrar</span></button></a>
+
+            </div>
+            
+        </header>
+    
+    <main class="cprincipal">
+    
+    <div class="card">
+            <h2 class="textcard1"><strong>A sua vida pode mudar agora!</strong></h2><br></br>
+            <div class="card2">
+                <p>Gostaria se tornar o proximo programador?</p>
+                <p> Domine as tecnologias utilizadas pelas 
+                    empresas mais inovadoras do mundo e encare seu novo desafio profissional, 
+                    evoluindo em comunidade com os melhores experts.
+                </p>
+            </div>
+    </div> 
+    
+    
+    </main>
+    
+     <footer class="pie">
+        <div>
+            <h2>Programas de Apoio</h2><br></br>
+                <img src={require('./components/imagenes/logo1.jpg' )}alt="logo1" className='logos'/>
+                <img src={require('./components/imagenes/logo2.jpg' )}alt="logo2" className='logos'/>
+                <img src={require('./components/imagenes/logo1.jpg' )}alt="logo1" className='logos'/>
+                <img src={require('./components/imagenes/logo2.jpg' )}alt="logo2" className='logos'/>
+        </div>
+        
+     </footer>
+    </body>
+   
+   
+  );
+}
+
+export default App;
