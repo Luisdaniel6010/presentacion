5c4bc23cc91f1d281f7eab288205ec363250c213
luis <6010ldaniel@gmail.com>
2022-12-14 22:44:48 -0300

    App.js

--- present/src/App.js
+++ present/src/App.js
@@ -1,56 +1,24 @@
-import React from 'react';
-
-import './home.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <body>
-
-        <header>
-        <div id="root" className="content">
-       
-            <a href="home.html"><button class="boton"><span>Home</span></button></a>
-
-            <a href="./components/catalogo/index.js"><button class="boton">
-            <span>Catalogo</span></button></a>
-
-            <a href="login.html"><button class="boton"><span>Entrar</span></button></a>
-
-            <a href="cadastro.html"><button class="boton"><span>Cadastrar</span></button></a>
-
-            </div>
-            
-        </header>
-    
-    <main class="cprincipal">
-    
-    <div class="card">
-            <h2 class="textcard1"><strong>A sua vida pode mudar agora!</strong></h2><br></br>
-            <div class="card2">
-                <p>Gostaria se tornar o proximo programador?</p>
-                <p> Domine as tecnologias utilizadas pelas 
-                    empresas mais inovadoras do mundo e encare seu novo desafio profissional, 
-                    evoluindo em comunidade com os melhores experts.
-                </p>
-            </div>
-    </div> 
-    
-    
-    </main>
-    
-     <footer class="pie">
-        <div>
-            <h2>Programas de Apoio</h2><br></br>
-                <img src={require('./components/imagenes/logo1.jpg' )}alt="logo1" className='logos'/>
-                <img src={require('./components/imagenes/logo2.jpg' )}alt="logo2" className='logos'/>
-                <img src={require('./components/imagenes/logo1.jpg' )}alt="logo1" className='logos'/>
-                <img src={require('./components/imagenes/logo2.jpg' )}alt="logo2" className='logos'/>
-        </div>
-        
-     </footer>
-    </body>
-   
-   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
