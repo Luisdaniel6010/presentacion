a13591aab7d4143b8cedeb2e19ffb20b0984b279
luis <6010ldaniel@gmail.com>
2022-12-14 21:57:20 -0300

    App.jsx

--- present/src/App.js
+++ present/src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routers, Route } from 'react-router-dom'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 import './home.css';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Catalogo from './pages/Catalogo';
-import Registro from './pages/Registro';
 
 
+ReactDOM.render(
+  <BrowserRouter>
+      <Login />
+      <Catalogo />
+  </ BrowserRouter>
+  , document.getElementById('root'));
+registerServiceWorker();
 
-
-function App() {
-  return (
-    
-    <Router>
-    <Routers>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/catalogo' element={<Catalogo/>}/>
-      <Route path='/Login' element={<Login/>}/>
-      <Route path='/Registro' element={<Registro/>}/>
-    </Routers>
-    </Router>
-   
-  );
-}
-
-export default App;
